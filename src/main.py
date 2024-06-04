a0d3183a5f5363266aaf931b338a5a59e08f497f
anotherAsad <ashaq.bee16seecs@seecs.edu.pk>
2024-06-04 21:18:49 +0000

    Hash Function Added

--- src/main.py
+++ src/main.py
@@ -8,23 +8,55 @@
 
 decompressed_stream = ""
 output_stream = ""
-search_buffer = "AYABRACADABRAABRALOUISISGAYABRACAABCABCABCABCABCABC"
+search_buffer = "AYABRACADABRAABRALOUISISGAYABRACAABCABCABCABCABCABCQWERT"
 
-string_idx = 0
-
-def stupid_match(string_idx):
-	global search_buffer
-
+def hash_match(string_idx):
 	MIN_MATCH_LEN = 4
 	MAX_MATCH_LEN = 255
-	MAX_SEARCH_DIST = 4
+	MAX_SEARCH_DIST = 255
 
-	possible_matches = []
+	# Set a static variable
+	if not hasattr(hash_match, "hash_map"):
+		hash_match.hash_map = {}
 
-	start_location = string_idx - MAX_SEARCH_DIST if string_idx > MAX_SEARCH_DIST else 0
+	hash_map = hash_match.hash_map
+	
+	# if the key doesn't exit, add it.
+	if not search_buffer[string_idx:string_idx+MIN_MATCH_LEN] in hash_map.keys():
+		hash_map[search_buffer[string_idx:string_idx+MIN_MATCH_LEN]] = string_idx
+
+		# Check for dictionary overflow and reconstrain it
+		if len(hash_map) > MAX_SEARCH_DIST:
+			del hash_map[list(hash_map.keys())[0]]
+
+		return (0, 0)
+	
+	# Otherwise start looking for the length of the match
+	match_idx = hash_map[search_buffer[string_idx:string_idx+MIN_MATCH_LEN]]
+
+	y = MIN_MATCH_LEN
+
+	for _ in range(0, MAX_MATCH_LEN+1):
+		if string_idx+y >= len(search_buffer):
+			break
+		elif search_buffer[match_idx+y] != search_buffer[string_idx+y]:
+			break
+		else:
+			y += 1
+
+	return (string_idx-match_idx, y)
+
+def stupid_match(string_idx):
+	MIN_MATCH_LEN = 4
+	MAX_MATCH_LEN = 255
+	MAX_SEARCH_DIST = 255
+
+	max_len_tup = (0, 0)
+
+	start_location = (string_idx - MAX_SEARCH_DIST) if string_idx > MAX_SEARCH_DIST else 0
 
 	# Step 1: Find the first character that matches
-	for x in range(start_location, string_idx-1):
+	for x in range(start_location, string_idx):
 		if search_buffer[string_idx] != search_buffer[x]:
 			continue
 
@@ -33,58 +65,55 @@ def stupid_match(string_idx):
 
 		# Clever trick: Allows for going past string idx, i.e. distance < length.
 		# WARN: String out-of-range guard not included. Python is immune to this, but heed in other languages.
-		for _ in range(0, MAX_MATCH_LEN):
-			if search_buffer[string_idx:string_idx+y] != search_buffer[x:x+y]:
+		for _ in range(0, MAX_MATCH_LEN+1):
+			if search_buffer[string_idx+y] != search_buffer[x+y]:
 				break
 			else:
 				y += 1
 
+		if y > max_len_tup[1]:
+			max_len_tup = (string_idx-x, y)
+
 		# Append a tuple (back-distance, length)
-		possible_matches.append((string_idx-x, y-1))
 
-	# Step 3: Find the match with max length and return it.
-	if not possible_matches:
-		return (0, 0)
-	
-	max_len_tup = (possible_matches[0][0], possible_matches[0][1])
-
-	for tup in possible_matches:
-		if tup[1] > max_len_tup[1]:
-			max_len_tup = (tup[0], tup[1])
-
+	# Step 3: Return a valid search result
 	if max_len_tup[1] < MIN_MATCH_LEN:
 		return (0, 0)
 	else:
 		return max_len_tup
 
+
 def emit(inp):
 	global output_stream
+	PRINT_MODE = False
 
 	if type(inp) == type((0, 0)):
 		output_stream += "!"+str(chr(inp[0]))+str(chr(inp[1]))
+		if PRINT_MODE: print("!", ord(str(chr(inp[0]))),ord(str(chr(inp[1]))), end=" ", sep=" ")
 	elif inp == "!":
 		output_stream += "!!"
+		if PRINT_MODE: print("!!", end="")
 	else:
 		output_stream += inp
+		if PRINT_MODE: print(ord(inp), end=" ")
 
 	return
 
 
 def LZcompress():
-	global sliding_window, string_idx
-	match_func = stupid_match
+	match_func = hash_match
+	string_idx = 0
 
 	while string_idx < len(search_buffer):
 		ld_pair = match_func(string_idx)
 		
-		if(ld_pair != (0, 0)):
+		# For valid (d, l) pair and 'd' not matching '!' character
+		if (ld_pair != (0, 0)) and ld_pair[0] != 33:
 			emit(ld_pair)
-			print(ld_pair, end="")
 			string_idx += ld_pair[1]
 			continue
 		
 		emit(search_buffer[string_idx])
-		print(search_buffer[string_idx], end="")
 
 		string_idx += 1
 
@@ -114,15 +143,51 @@ def LZdecompress():
 
 	return
 
-	
+# Read a file into a string
+with open("../sample_file.txt", "r") as f:  # Open the file in read mode ("r")
+	search_buffer = f.read()  # Read the entire content into a string
 
 LZcompress()
 LZdecompress()
 
-print("\n")
+if search_buffer == decompressed_stream:
+	print("Decompression Successful, matches with source.")
+	print(f"Size of Original: {len(search_buffer)}. Size of comressed: {len(output_stream)}")
+	print(f"Compression Ratio: {len(search_buffer)/len(output_stream):.3f}")
+	print(f"Size Reduction: {(1-len(output_stream)/len(search_buffer))*100:.2f} %")
+else:
+	if False:
+		same_count = 0
+		mismatch_idx_list = []
 
-print("ORIGINAL:")
-print(search_buffer)
-print("DECOMPRESSED:")
-print(decompressed_stream)
+		for x in range(0, min(len(search_buffer), len(decompressed_stream))):
+			if search_buffer[x] == decompressed_stream[x]:
+				same_count += 1
+			else:
+				mismatch_idx_list.append(x)
+				
 
+		print(f"Decompression Failed, Match count: {same_count} {same_count/len(search_buffer)*100:.3f}%.")
+		print(f"Mismatches at: {mismatch_idx_list}")
+		
+		a = mismatch_idx_list[0]
+
+		print("ORIGINAL: ", end="")
+
+		for x in range(-1, 10):
+			print(ord(search_buffer[a+x]), end=" ")
+		
+		print("\nDECOMPRE: ", end="")
+
+		for x in range(-1, 10):
+			print(ord(decompressed_stream[a+x]), end=" ")
+	else:
+		print("\nORIGINAL:")
+		for x in range(0, len(search_buffer)):
+			print(ord(search_buffer[x]), end = " ")
+		print("\nDECOMPRE:")
+		for x in range(0, len(decompressed_stream)):
+			print(ord(decompressed_stream[x]), end = " ")
+
+
+print()
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+#![allow(non_snake_case)]
+
+use std::fs;
+
+fn stupid_match(search_buffer: String, search_loc: usize) -> (i32, i32) {
+	let MIN_MATCH_LEN = 4;
+	let MAX_MATCH_LEN = 256;
+	let MAX_SEARCH_DIST = 256;
+
+    let mut possible_matches: Vec<(i32, i32)> = Vec::new();
+
+    // Decide start location of the search_window
+    let start_loc = match search_loc > MAX_SEARCH_DIST {
+        true  => search_loc - MAX_SEARCH_DIST,
+        false => 0,
+    };
+
+    for i in start_loc..search_loc {
+        // Step 1: find the first character that matches
+        if search_buffer.chars().nth(i).unwrap() !=  search_buffer.chars().nth(search_loc).unwrap() {
+            continue;
+        }
+
+        // Step 2: If match is found, find the length of the match
+        let mut y = 1
+
+        for _ 0..MAX_MATCH_LEN
+    }
+
+    return (0, 0);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let bytes = fs::read("../sample_file.txt").unwrap();
+    // Process the bytes here
+    println!("Read {} bytes from the file", bytes.len());
+
+
+    let block_len = if bytes.len() > 4096 {4096} else {bytes.len()};
+
+    // declare a string
+    let search_buffer = String::from_utf8(bytes[..block_len].to_vec()).unwrap();
+
+    stupid_match(search_buffer, 0);
 }
